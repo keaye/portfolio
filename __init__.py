7ee053f6b91b867178091fe55795bb71f438accc
Ayomide Adeniyi <ayomadeniyi@outlook.com>
2024-06-24 19:07:50 -0700

    Add files via upload

---
+++ __init__.py
@@ -0,0 +1,58 @@
+from flask import Flask, render_template, abort
+
+app = Flask(__name__)
+
+
+
+projects = [
+    {
+        "name": "Blog-entry Form with Pythonand MongoDB",
+        "thumb": "img/blog.jpg",
+        "hero": "img/blog-hero.jpg",
+        "categories": ["flask", "python"],
+        "slug": "blog-page",
+        "prod": "https://microblog-gux0.onrender.com",
+    },
+    {
+        "name": "FizzBuzz with Python",
+        "thumb": "img/fizzbuzz.jpg",
+        "hero": "img/fizz.png",
+        "categories": ["flask", "python"],
+        "slug": "fizz-buzz",
+        "prod": "https://testss-rosy.vercel.app/"
+    }
+]
+
+slug_to_project = {project["slug"]: project for project in projects}
+
+
+
+@app.route("/")
+def home():
+    return render_template("home.html", projects=projects)
+
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
+
+
+@app.route("/project/<string:slug>")
+def project(slug):
+    if slug not in slug_to_project:
+        abort(404)
+    return render_template(f"project_{slug}.html", project=slug_to_project[slug])
+
+
+
+
+
+
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template("404.html"), 404
