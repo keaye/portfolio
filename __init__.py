a97bf353028ddee0e659e85fafeebbfc2a4f3408
Ayomide Adeniyi <ayomadeniyi@outlook.com>
2024-07-10 17:36:46 -0700

    modified:   __init__.py 	new file:   __pycache__/__init__.cpython-312.pyc 	new file:   static/img/tracker-hero.png 	new file:   static/img/tracker.jpg 	new file:   templates/project_habit-tracker.html

--- __init__.py
+++ __init__.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 projects = [
     {
-        "name": "Blog-entry Form with Python and MongoDB",
+        "name": "Blog-entry Form with Python",
         "thumb": "img/blog.jpg",
         "hero": "img/blog-hero.jpg",
         "categories": ["flask", "python"],
@@ -20,7 +20,15 @@ projects = [
         "categories": ["flask", "python"],
         "slug": "fizz-buzz",
         "prod": "https://testss-rosy.vercel.app/"
-    }
+    },
+    {
+        "name": " Habit-tracker with Python",
+        "thumb": "img/tracker.jpg",
+        "hero": "img/tracker-hero.png",
+        "categories": ["flask", "python"],
+        "slug": "habit-tracker",
+        "prod": "https://habit-i6s1.onrender.com"
+    },
 ]
 
 slug_to_project = {project["slug"]: project for project in projects}
